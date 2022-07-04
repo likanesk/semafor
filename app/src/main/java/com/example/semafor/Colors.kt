a10068680e6ed351f08198591cd2c33f8a91182f
Adrián Ihring <adrian.ihring@gmail.com>
2022-07-04 18:44:15 +0200

    create enum Colors

---
+++ app/src/main/java/com/example/semafor/Colors.kt
@@ -0,0 +1,11 @@
+package com.example.semafor
+
+import android.graphics.Color
+
+enum class Colors(val rgb: String) {
+        GREEN("#008000"),
+        ORANGE("#FFA500"),
+        RED("#FF0000"),
+        GRAY("#D3D3D3");
+}
+
