a4cb8cedebb5cb512fb7c8a0e57b6224c9f17473
Adrián Ihring <adrian.ihring@gmail.com>
2022-07-04 18:44:39 +0200

    create NightMode and set function

--- app/src/main/java/com/example/semafor/MainActivity.kt
+++ app/src/main/java/com/example/semafor/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.semafor
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+import android.os.Handler
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nightMode: NightMode = NightMode()
+
+        /*pauseButton.setOnClickListener() {
+            repeat(10) {
+                nightMode.setNightMode(orangeView)
+            }
+        }*/
+
+        pauseButton.setOnClickListener() {
+            nightMode.setNightMode(orangeView)
+        }
+
+
+
     }
+
 }
+
