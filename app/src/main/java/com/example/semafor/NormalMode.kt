6cb58fa6ae81ed3f3ef1e4e48a4829374619caba
Adrián Ihring <adrian.ihring@gmail.com>
2022-07-05 15:47:20 +0200

    add Runnable for lights

--- app/src/main/java/com/example/semafor/NormalMode.kt
+++ app/src/main/java/com/example/semafor/NormalMode.kt
@@ -11,13 +11,50 @@ class NormalMode(redView: ImageView, orangeView: ImageView, greenView: ImageView
     override var orange: ImageView = orangeView
     override var green: ImageView = greenView
 
+    fun start() {
+        runnable.run()
+    }
+
+    var runnable: Runnable = object : Runnable{
+        override fun run() {
+            runnable1.run()
+            runnable2.run()
+            runnable3.run()
+        }
+    }
+
+    var runnable1: Runnable = object : Runnable {
+        override fun run() {
+            red.setColorFilter(colorRed)
+            handler.postDelayed(
+                {
+                    red.setColorFilter(colorRed)
+                    orange.setColorFilter(colorOrange)
+                }, delay
+            )
+            delay += 2000
+        }
+    }
+
+    var runnable2: Runnable = object : Runnable {
+        override fun run() {
+            handler.postDelayed({
+                red.setColorFilter(colorGray)
+                orange.setColorFilter(colorGray)
+                green.setColorFilter(colorGreen)
+            }, delay)
+            delay += 5000
+        }
+    }
+
+    var runnable3: Runnable = object : Runnable {
+        override fun run() {
             handler.postDelayed({
                 red.setColorFilter(colorGray)
                 orange.setColorFilter(colorOrange)
                 green.setColorFilter(colorGray)
             }, delay)
-            delay += 5000
+            delay = 5000
         }
-
     }
 }
