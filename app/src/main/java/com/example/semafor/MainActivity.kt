14c1eae22e951c01c66729dde038fbc1f9e5d258
Adrián Ihring <adrian.ihring@gmail.com>
2022-07-05 21:12:19 +0200

    changing buttons colour

--- app/src/main/java/com/example/semafor/MainActivity.kt
+++ app/src/main/java/com/example/semafor/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.semafor
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
